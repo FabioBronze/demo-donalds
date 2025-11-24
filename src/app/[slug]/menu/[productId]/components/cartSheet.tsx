b43defedbab563ef272b057bf74c12c95288d350
Fábio Bronze <fabio.r.bronze@hotmail.com>
2025-11-24 14:07:35 +0000

    feat: ensure cart total is shown

--- src/app/[slug]/menu/[productId]/components/cartSheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cartSheet.tsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { formatCurrency } from "@/helpers/format-currency";
 
 import CartItem from "../../components/cartItem";
 import { CartContext } from "../../context/cart";
 
 const CartSheet = () => {
-    const { isOpen, toggleCart, products } = useContext(CartContext)
+    const { isOpen, toggleCart, products, total } = useContext(CartContext)
 
     return (
         <Sheet open={isOpen} onOpenChange={toggleCart}>
@@ -14,10 +17,21 @@ const CartSheet = () => {
                 <SheetHeader>
                     <SheetTitle className="text-left">Cart</SheetTitle>
                 </SheetHeader>
-                <div className="py-5">
-                    {products.map((product) => (
-                        <CartItem key={product.id} product={product} />
-                    ))}
+                <div className="py-5 flex h-full flex-col">
+                    <div className="flex-auto">
+                        {products.map((product) => (
+                            <CartItem key={product.id} product={product} />
+                        ))}
+                        <Button className="w-full rounded-full">Complete Purchase</Button>
+                    </div>
+                    <Card className="mb-6">
+                        <CardContent className="p-5">
+                            <div className="flex justify-between">
+                                <p className="text-sm text-muted-foreground">Total</p>
+                                <p className="font-semibold text-sm">{formatCurrency(total)}</p>
+                            </div>
+                        </CardContent>
+                    </Card>
                 </div>
             </SheetContent>
         </Sheet>
