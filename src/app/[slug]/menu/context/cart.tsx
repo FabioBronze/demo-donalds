ed715ecae0546bd3825f95f1b13f9c1e3e0888cf
Fábio Bronze <fabio.r.bronze@hotmail.com>
2025-11-24 13:45:55 +0000

    feat: ensure product quantity can be removed

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -14,6 +14,7 @@ export interface ICartContext {
     addProduct: (product: CartProduct) => void
     decreaseProductQuantity: (productId: string) => void
     increaseProductQuantity: (productId: string) => void
+    removeProduct: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -22,7 +23,8 @@ export const CartContext = createContext<ICartContext>({
     toggleCart: () => { },
     addProduct: () => { },
     decreaseProductQuantity: () => { },
-    increaseProductQuantity: () => { }
+    increaseProductQuantity: () => { },
+    removeProduct: () => { }
 })
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -76,6 +78,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
             })
         })
     }
+    const removeProduct = (productId: string) => {
+        setProducts(prevProducts => prevProducts.filter(prevProduct => prevProduct.id !== productId))
+    }
     return (
         <CartContext.Provider value={{
             isOpen,
@@ -83,7 +88,8 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
             toggleCart,
             addProduct,
             decreaseProductQuantity,
-            increaseProductQuantity
+            increaseProductQuantity,
+            removeProduct
         }}
         >
             {children}
